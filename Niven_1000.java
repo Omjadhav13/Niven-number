12c4ef3e8e745c225c62a1c11772cf4c74c75769
Om Jadhav <74755805+Omjadhav13@users.noreply.github.com>
2023-10-11 17:55:34 +0530

    Add files via upload

---
+++ Niven_1000.java
@@ -0,0 +1,25 @@
+package spiders;
+
+public class Niven_1000 {
+
+	public static void main(String[] args) {
+		
+		int count=0;
+		for(int i=1;i<=1000;i++) {
+			int sum=0;
+			int n=i;
+			while(n!=0) {
+				int digit=n%10;
+				sum=sum+digit;
+				n=n/10;
+			}
+			if(i%sum==0) {
+				System.out.println(i);
+				count++;
+			}
+		}
+		System.out.println(count);
+
+	}
+
+}
---
+++ Niven_number.java
@@ -0,0 +1,26 @@
+package spiders;
+
+public class Niven_number {
+
+	public static void main(String[] args) {
+		int n=21;
+		int a=n;
+		int sum=0;
+		while(n!=0) {
+			int digit=n%10;
+			sum=sum+digit;
+			
+			n=n/10;
+		}if(a%sum==0) {
+			System.out.println("niven ");
+		}
+		else {
+			System.out.println("not");
+		}
+		
+		
+	
+
+	}
+
+}
